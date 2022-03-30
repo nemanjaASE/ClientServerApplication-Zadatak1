0978e194d5b730da92e88b7b0d287bf7263e9c9a
Nemanja Petrović <petrovic.pr44.2019@uns.ac.rs>
2022-03-30 14:16:15 +0200

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Client
+    {
+        public Client()
+        {
+
+        }
+
+        public void StartClient()
+        {
+            IPAddress ipAddress = IPAddress.Loopback;
+            IPEndPoint remoteEndpoint = 
+                new IPEndPoint(ipAddress,11000);
+
+            Socket socket =
+                new Socket(ipAddress.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(remoteEndpoint);
+                Console.WriteLine("Socket connected to " + socket.RemoteEndPoint.ToString());
+
+                NetworkStream stream = new NetworkStream(socket);
+                StreamReader sr = new StreamReader(stream);
+                StreamWriter sw = new StreamWriter(stream) { NewLine = "\r\n", AutoFlush = true};
+                string line = "";
+                do
+                {
+                    string input = Console.ReadLine();
+                    sw.WriteLine(input);
+                    Console.WriteLine("# From server: ");
+                    do
+                    {
+                        line = sr.ReadLine();
+                        if(!line.Equals("END"))
+                            Console.WriteLine(line);
+                    } while (!line.Equals("END"));
+
+                } while (!line.Equals("EXIT"));
+
+                sw.Close();
+                sr.Close();
+                stream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Server
+    {
+        private static List<string> users = new List<string>();
+        public Server()
+        {
+                
+        }
+        
+        public void ServerListening()
+        {
+            IPAddress ipAddress = IPAddress.Loopback;
+            IPEndPoint localEndpoint = 
+                new IPEndPoint(ipAddress,11000);
+
+            Socket serverSocket =
+                new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                serverSocket.Bind(localEndpoint);
+                serverSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    Socket socket = serverSocket.Accept();
+
+                    Task<int> task = Task.Factory.StartNew(() => Run(socket));
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+
+        private int Run(Socket socket)
+        {
+            NetworkStream stream = new NetworkStream(socket);
+            StreamReader sr = new StreamReader(stream);
+            StreamWriter sw = new StreamWriter(stream) { NewLine = "\r\n", AutoFlush = true};
+
+            string[] delim = { ",", " ", "  " };
+            try
+            {
+                string msg = "";
+                do
+                {
+                    string line;
+                    try
+                    {
+                        line = sr.ReadLine();
+                        Console.WriteLine("# From client: " + line);
+                    }
+                    catch (Exception)
+                    {
+                        sw.Close();
+                        sr.Close();
+                        stream.Close();
+
+                        socket.Shutdown(SocketShutdown.Both);
+                        socket.Close();
+                        return -1;
+                    }
+
+                    
+                    string[] tokens = line.Split(delim, System.StringSplitOptions.RemoveEmptyEntries);
+
+                    msg = "";
+                    if (tokens[0].Equals("EXIT"))
+                    {
+                        msg = "EXIT\n";
+                    }else if(tokens[0].Contains("ADD") && tokens.Length == 2)
+                    {
+                        if(!users.Contains(tokens[1]))
+                        {
+                            users.Add(tokens[1]);
+                            msg = "Korisnik "+ tokens[1] +" uspesno dodat.\n";
+                        }
+                        else
+                        {
+                            msg = "Korisnik " + tokens[1] + " vec postoji.\n";
+                        }
+                    }else if(tokens[0].Contains("LIST") && tokens.Length == 1)
+                    {
+                        if (users.Count == 0)
+                            msg = "<empty list>\n";
+                        foreach (string user in users)
+                        {
+                            msg += (user + "\n");
+                        }
+                        
+                    }else if(tokens[0].Contains("REMOVE") && tokens.Length == 2)
+                    {
+                        if (users.Contains(tokens[1]))
+                        {
+                            users.Remove(tokens[1]);
+                            msg = "Korisnik " + tokens[1] + " uspesno obrisan.\n";
+                        }
+                        else
+                        {
+                            msg = "Korisnik " + tokens[1] + " ne postoji.\n";
+                        }
+                    }else if (tokens[0].Contains("FIND") && tokens.Length == 2)
+                    {
+                        if (users.Contains(tokens[1]))
+                        {
+                            msg = "Korisnik " + tokens[1] + " je pronadjen.\n";
+                        }
+                        else
+                        {
+                            msg = "Korisnik " + tokens[1] + " ne postoji.\n";
+                        }
+                    }else if(tokens[0].Contains("ADD") && tokens.Length > 2)
+                    {
+                        string uspesno = "";
+                        string neuspesno = "";
+                        for (int i = 1; i < tokens.Length; i++)
+                        {
+                            if(users.Contains(tokens[i]))
+                            {
+                                neuspesno +=  tokens[i] + " ";
+                            }else
+                            {
+                                users.Add(tokens[i]);
+                                uspesno +=  tokens[i] + " ";
+                            }
+                        }
+                        if (uspesno.Equals(""))
+                            uspesno = "<empty>";
+                        if (neuspesno.Equals(""))
+                            neuspesno = "<empty>";
+                        msg = "Uspesno: " + uspesno + "\nNeuspesno: " + neuspesno + "\n";
+                    }
+                    else
+                    {
+                        msg = "Pogresna komanda!\n";
+                    }
+
+                    sw.WriteLine(msg + "END");
+
+                } while (!msg.Equals("EXIT"));
+                
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            sw.Close();
+            sr.Close();
+            stream.Close();
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+            return 0;
+        }
+    }
+}
